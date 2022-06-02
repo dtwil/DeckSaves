5285661cd63b780d19fbb9821345b6f0413b5704
dtwil <danieltwili@gmail.com>
2022-06-02 18:59:19 -0400

    Implement uploading script

---
+++ upload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+. decksaves.config
+
+zip -r saves.zip saves/
+.$rclone_path copy saves.zip remote:$remote_path --progress > log.txt 2&>1
+if [ $? -eq 0 ]; then
+    zenity --info \
+        --text="Successfully backed up saves!" \
+        --width=250
+else
+    zenity --error \
+        --text="Failed to back up saves. See log.txt for details." \
+        --width=250
+fi
