bec27c5132adb6a0e7ca9f91017e576d0ec7e532
dtwil <danieltwili@gmail.com>
2022-06-02 18:58:49 -0400

    Make install script generate config file

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DOWNLOAD_LINK="https://downloads.rclone.org/rclone-current-linux-amd64.zip"
 WELCOME_MSG="Welcome to DeckSaves!\n\nTo start, choose the folder where you want to install DeckSaves."
-SUCCESS_MSG="Install complete!\n Please configure rclone (see DeckSave's GitHub for instructions)."
+SUCCESS_MSG="Install complete!\n\nPlease configure rclone (see DeckSave's GitHub for instructions)."
 
 # Display welcome message
 zenity --info \
@@ -35,17 +35,19 @@ else
     exit 1
 fi
 
-# Remove the rclone installer
+# Remove the rclone installer and make saves folder
 if [ $? -eq 0 ]; then
     rm rclone.zip
-else
-    zenity --error \
-        --text="Unzipping rclone failed. Do you have unzip installed?"
-        --width=250
-    exit 1
+    mkdir saves
 fi
 
-mkdir saves
+# Create config file
+touch decksaves.config
+echo "#!/bin/bash" >> decksaves.config
+echo "rclone_path=/rclone/rclone" >> decksaves.config
+echo "remote_path=DeckSaves" >> decksaves.config
+chmod +x decksaves.config
+
 zenity --info \
     --text="$SUCCESS_MSG" \
     --width=250
