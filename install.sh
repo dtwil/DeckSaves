6c0388af177a49a856c8ce679c50ba177f3d58e9
dtwil <dantwili@Dans-MacBook-Air.local>
2022-06-05 11:36:19 -0400

    Add titles to dialogs in install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+TITLE="DeckSaves"
 DOWNLOAD_LINK="https://downloads.rclone.org/rclone-current-linux-amd64.zip"
 WELCOME_MSG="Welcome to DeckSaves!\n\nDeckSaves will be installed to this directory."
 SUCCESS_MSG="Install complete!\n\nRun decksaves.sh to add save folders and back them up."
 
 # Display welcome message
 zenity --info \
+    --title="$TITLE" \
     --text="$WELCOME_MSG" \
     --width=250
 
@@ -13,6 +15,7 @@ if [ $? -eq 0 ]; then
     curl -o rclone.zip $DOWNLOAD_LINK
 else
     zenity --error \
+        --title="$TITLE" \
         --text="DeckSave was not installed." \
         --width=250
     exit 1
@@ -23,6 +26,7 @@ if [ $? -eq 0 ]; then
     unzip -j -o rclone.zip -d rclone/
 else
     zenity --error \
+        --title="$TITLE" \
         --text="Downloading rclone failed." \
         --width=250
     exit 1
@@ -44,6 +48,7 @@ chmod +x decksaves.config
 
 # Ask for cloud storage provider
 cloud_storage=$(zenity --list \
+    --title="$TITLE" \
     --text="Select your cloud storage provider below:" \
     --column="Provider" \
         "Google Drive" \
@@ -59,7 +64,8 @@ cloud_storage=$(case "$cloud_storage" in
 echo "cloud_storage=$cloud_storage" >> decksaves.config
 if [ $? -ne 0 ]; then
     zenity --error \
-        --text="DeckSave did not finish setup. Try running the installer again."
+        --title="$TITLE" \
+        --text="DeckSave did not finish setup. Try running the installer again." \
         --width=250
     exit 1
 
@@ -70,10 +76,12 @@ timeout 1m .$rclone_path config create $remote_name $cloud_storage
 # Show success dialog
 if [ $? -eq 0 ]; then
     zenity --info \
+        --title="$TITLE" \
         --text="$SUCCESS_MSG" \
         --width=250
 else
     zenity --error \
+        --title="$TITLE" \
         --text="DeckSave did not finish setup. Try running the installer again." \
         --width=250
 fi
